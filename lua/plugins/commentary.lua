d5464666d1b8dd5f72c324860746ce2d1d7aa1a4
BurkeStrang <burke.strang@outlook.com>
2024-04-17 18:49:21 -0400

    updaing git signs and commentary

--- lua/plugins/commentary.lua
+++ lua/plugins/commentary.lua
@@ -1,9 +1,20 @@
 return {
-  {
-    "numToStr/Comment.nvim",
-    opts = {
-      -- add any options here
-    },
-    lazy = false,
+  "numToStr/Comment.nvim",
+  dependencies = "JoosepAlviste/nvim-ts-context-commentstring",
+  keys = {
+    { "gcc", mode = "n" },
+    { "gbc", mode = "n" },
+
+    { "gc", mode = { "n", "v" } },
+    { "gb", mode = { "n", "v" } },
+
+    { "gco", mode = "n" },
+    { "gcA", mode = "n" },
+    { "gcO", mode = "n" },
   },
+  config = function()
+    require("Comment").setup({
+      pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+    })
+  end,
 }
