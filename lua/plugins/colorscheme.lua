4b2887da99bd6dbd8993eb6c1b67f79572aa8fa6
Burke.Strang <burke.strang@vertigis.com>
2024-06-10 15:42:46 -0400

    update colors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -17,16 +17,39 @@ local palettes = {
 local specs = {
   all = {
     syntax = {
+      func = "cyan",
       -- keyword = "red",
       -- operator = "green",
       -- conditional = "red",
       -- number = "magenta",
-      variable = { base = "#d8b897", bright = "#e3bfa1", dim = "#bfa17a" },
-      -- constant = "magenta",
+      variable = "#c8b38a",
       -- string = "magenta",
       -- type = "pink",
       -- preproc = "red",
       comment = "#556378",
+
+      -- this is the default for carbonfox
+      -- bracket     = spec.fg2,           -- Brackets and Punctuation
+      -- builtin0    = pal.red.base,       -- Builtin variable
+      -- builtin1    = pal.cyan.bright,    -- Builtin type
+      -- builtin2    = pal.orange.bright,  -- Builtin const
+      -- builtin3    = pal.red.bright,     -- Not used
+      -- comment     = pal.comment,        -- Comment
+      -- conditional = pal.magenta.bright, -- Conditional and loop
+      -- const       = pal.orange.bright,  -- Constants, imports and booleans
+      -- dep         = spec.fg3,           -- Deprecated
+      -- field       = pal.blue.base,      -- Field
+      -- func        = pal.blue.bright,    -- Functions and Titles
+      -- ident       = pal.cyan.base,      -- Identifiers
+      -- keyword     = pal.magenta.base,   -- Keywords
+      -- number      = pal.orange.base,    -- Numbers
+      -- operator    = spec.fg2,           -- Operators
+      -- preproc     = pal.pink.bright,    -- PreProc
+      -- regex       = pal.yellow.bright,  -- Regex
+      -- statement   = pal.magenta.base,   -- Statements
+      -- string      = pal.green.base,     -- Strings
+      -- type        = pal.yellow.base,    -- Types
+      -- variable    = pal.white.base,     -- Variables
     },
   },
 }
