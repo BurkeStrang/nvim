3ef843bb8e8222898be16fbe0e4aea3b8b95f07b
Burke.Strang <burke.strang@vertigis.com>
2024-06-12 11:44:37 -0400

    seeing if will work in multiple environments

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,7 +6,7 @@ return {
     { "folke/neodev.nvim", opts = {} },
     "mason.nvim",
     "williamboman/mason-lspconfig.nvim",
-    "seblj/roslyn.nvim"
+    "seblj/roslyn.nvim",
   },
   ---@class PluginLspOpts
   opts = function()
@@ -99,9 +99,9 @@ return {
       -- return true if you don't want this server to be setup with lspconfig
       ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
       setup = {
-			require("roslyn").setup({
-				capabilities = nil,
-			})
+        require("roslyn").setup({
+          capabilities = nil,
+        }),
         -- example to setup with typescript.nvim
         -- tsserver = function(_, opts)
         --   require("typescript").setup({ server = opts })
