c159b7a288c1775e21ad85eb0e681f028b4fbbf7
BurkeStrang <burke.strang@outlook.com>
2024-06-28 17:42:20 -0400

    custom snippets

--- lua/plugins/vimsnip.lua
+++ lua/plugins/vimsnip.lua
@@ -3,6 +3,7 @@ return {
     "hrsh7th/vim-vsnip",
     config = function()
       -- You can add additional configuration here if needed
+      vim.g.vsnip_snippet_dir = vim.fn.stdpath("config") .. "/lua/custom/snippets"
     end,
   },
   -- Other plugins...
