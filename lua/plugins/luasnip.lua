634ac2170a8874a4ab1715e735594a29044e428a
BurkeStrang <burke.strang@outlook.com>
2024-06-28 22:19:37 -0400

    config

--- lua/plugins/luasnip.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "L3MON4D3/LuaSnip",
-  version = "v2.*", -- Follow latest release
-  build = "make install_jsregexp", -- Install jsregexp (optional)
-  config = function()
-    require("luasnip.loaders.from_vscode").lazy_load({ paths = { ".custom/my-cool-snippets" } })
-  end,
-}
